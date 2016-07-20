e6fc5d24bd09c821e2fb19fca563e27e4d69f70a
Steven McGrath <mcgrath.steven7@gmail.com>
2016-07-19 21:09:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+var kittens = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten);
+}
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+function appendKitten(kitten) {
+  new_kittens = [...kittens, kitten];
+  return new_kittens;
+}
+function prependKitten(kitten) {
+  new_kittens = [kitten, ...kittens];
+  return new_kittens;
+}
+function removeLastKitten() {
+  new_kittens = kittens.slice(0, kittens.length - 1);
+  return new_kittens;
+}
+function removeFirstKitten() {
+  new_kittens = kittens.slice(1);
+  return new_kittens;
+}
